acac1dc3ac14a5db5fa6161e05c3a6adad5b3a48
Emre Özcan <justsomechars@gmail.com>
2020-05-19 01:28:46 +0300

    Moved scripts from /src/additional to /src/scripts.

--- src/manage.py
+++ src/manage.py
@@ -2,7 +2,7 @@ import importlib
 import pathlib
 import argparse
 
-modules = pathlib.Path(__file__).parent.absolute().joinpath("additional").glob("*.py")
+modules = pathlib.Path(__file__).parent.absolute().joinpath("scripts").glob("*.py")
 names = [module.name for module in modules]
 
 parser = argparse.ArgumentParser(add_help=False)
@@ -14,5 +14,5 @@ if f"{args[0].script}.py" not in names:
     print("No script exists with that name!")
     exit(1)
 
-module = importlib.import_module(f"additional.{args[0].script}")
+module = importlib.import_module(f"scripts.{args[0].script}")
 module.call(args[0].script, args[1])
